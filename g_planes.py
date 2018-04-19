9471023ae40cbcce0a094e9e3b391b8a58f3c304
Michael <co_eus@posteo.org>
2018-04-19 22:41:27 +0200

    added g_planes

---
+++ g_planes.py
@@ -0,0 +1,34 @@
+# modules
+import numpy as np
+# from cube_utils import *
+# from scipy.signal import sawtooth
+
+
+class g_planes():
+    '''
+    Generator: planes
+
+
+    '''
+
+    def __init__(self):
+        self.speed = 10
+        self.dir = 1
+
+    def control(self, speed, dir, dump):
+        self.speed = int(speed*9)
+        self.dir = int(dir*4)
+
+
+    def generate(self, step, dumpworld):
+        world = np.zeros([3, 10, 10, 10])
+        position = int( round((np.sin(0.1*step*self.speed)+1)*4.5))
+
+        if dir == 0:
+            world[position,:,:] = 1.0
+        elif dir == 1:
+            world[:, position,:] = 1.0
+        else:
+            world[:,:,position] = 1.0
+
+        return world
