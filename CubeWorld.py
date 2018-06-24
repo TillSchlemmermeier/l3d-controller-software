d8706a30009f1b82531c8a2af89f8bd72f7125a2
Michael <co_eus@posteo.org>
2018-06-24 22:06:53 +0200

    fixed rain, corrected orientation

--- CubeWorld.py
+++ CubeWorld.py
@@ -20,6 +20,8 @@ from generators.g_planes_falling import g_planes_falling
 from generators.g_orbiter2 import g_orbiter2
 from generators.g_wavepattern import g_wavepattern
 from generators.g_randomcross import g_randomcross
+from generators.g_growing_corner import g_growing_corner
+from generators.g_rain import g_rain
 #load effect modules
 from effects.e_blank import e_blank
 from effects.e_fade2blue import e_fade2blue
--- generators/g_growing_corner.py
+++ generators/g_growing_corner.py
@@ -1,6 +1,7 @@
 # modules
 import numpy as np
 from scipy.signal import sawtooth
+from random import randint
 from generators.g_genhsphere import gen_hsphere
 
 # fortran routine is in g_growing_sphere_f.f90
@@ -21,6 +22,11 @@ class g_growing_corner():
         self.maxsize = 10
         self.growspeed = 1
         self.steps = 0
+        self.counter = 0
+
+        self.xpos = 0
+        self.ypos = 0
+        self.zpos = 0
 
     def label(self):
         return ['maxsize',round(self.maxsize,2),'growspeed',round(self.growspeed,2),'Steps',round(self.steps,2)]
@@ -28,24 +34,36 @@ class g_growing_corner():
     def control(self, maxsize, growspeed, steps):
         self.maxsize = maxsize*10
         self.growspeed = growspeed
-        self.steps = steps
+        self.steps = steps*20
 
-    xpos = 9*randint(0,1)
-    ypos = 9*randint(0,1)
-    zpos = 9*randint(0,1)
+    def generate(self, step, dumpworld):
 
-    for x in range (0,steps)
-        def generate(self, step, dumpworld):
-            world = np.zeros([3, 10, 10, 10])
+        world = np.zeros([3, 10, 10, 10])
 
-            # oscillates between 0 and 1
-            osci = np.sin(step*self.growspeed)*0.5 + 1
+        # check for new calculation
+        if self.counter > self.steps:
+            self.xpos = 9*randint(0,1)
+            self.ypos = 9*randint(0,1)
+            self.zpos = 9*randint(0,1)
 
-            # scales to maxsize
-            size = self.maxsize * osci
-            # creates hollow sphere with parameters
-            world[0, :, :, :] = gen_hsphere(size,xpos,ypos,zpos)
-            world[1:, :, :, :] = world[0, :, :, :]
-            world[2:, :, :, :] = world[0, :, :, :]
+            self.counter = 0
 
-            return np.round(np.clip(world, 0, 1), 3)
+        # calculate radius
+        # oscillates between 0 and 1
+        osci = np.sin(self.counter*self.growspeed)*0.5 + 1
+
+        # scales to maxsize
+        size = self.maxsize * osci
+
+        x = self.xpos
+        y = self.ypos
+        z = self.zpos
+
+        # creates hollow sphere with parameters
+        world[0, :, :, :] = gen_hsphere(size,x,y,z)
+        world[1:, :, :, :] = world[0, :, :, :]
+        world[2:, :, :, :] = world[0, :, :, :]
+
+        self.counter += 1
+
+        return np.round(np.clip(world, 0, 1), 3)
