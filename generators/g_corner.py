573cb83b578700b39ac5d141a2f7cba99a4c8d3c
Yannig <yannig@gmx.net>
2018-06-25 03:04:19 +0200

    generator corner

--- generators/g_corner.py
+++ generators/g_corner.py
@@ -7,26 +7,26 @@ class g_corner():
     '''
 
     def __init__(self):
-        self.blub = 0
+        self.size = 0
 
-    def control(self, blub0, blub1, blub2):
-        self.blub = 0
+    def control(self, size, blub1, blub2):
+        self.size = size*5
 
     def label(self):
-        return ['empty','empty','empty','empty','empty','empty']
+        return ['size',round(self.maxsize,2),'empty','empty','empty','empty','empty']
 
     def generate(self, step, dumpworld):
         # create world
         world = np.zeros([3, 10, 10, 10])
 
         # switch on corners
-        world[:,0,0,0] = 1.0
-        world[:,0,0,9] = 1.0
-        world[:,0,9,0] = 1.0
-        world[:,9,0,0] = 1.0
-        world[:,0,9,9] = 1.0
-        world[:,9,0,9] = 1.0
-        world[:,9,9,0] = 1.0
-        world[:,9,9,9] = 1.0
+        world[:,0:size,0:size,0:size] = 1.0
+        world[:,0:size,0:size,9:9-size] = 1.0
+        world[:,0:size,9:9-size,0:size] = 1.0
+        world[:,9:9-size,0:size,0:size] = 1.0
+        world[:,0:size,9:9-size,9:9-size] = 1.0
+        world[:,9:9-size,0:size,9:9-size] = 1.0
+        world[:,9:9-size,9:9-size,0:size] = 1.0
+        world[:,9:9-size,9:9-size,9:9-size] = 1.0
 
         return np.clip(world, 0, 1)
