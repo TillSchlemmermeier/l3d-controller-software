addc76c4edd62431db06609174ec2088468061d6
Till_Schlemmermeier <till.schlemmermeier@gmail.com>
2018-06-26 15:40:25 +0200

    added g_gauss

--- generators/g_gauss.py
+++ generators/g_gauss.py
@@ -7,35 +7,41 @@ from scipy.stats import multivariate_normal
 class g_gauss():
 
     def __init__(self):
-        self.sigma = 0
+        self.sigma = 1
+        self.amplitude = 30
+        self.speed = 1
 
-    def control(self, sigma, blub, blub0):
-        self.sigma = round(sigma*1.2)
+    def control(self, sigma, amplitude, speed):
+        self.sigma = sigma*5+1
+        self.amplitude = amplitude*50
+        self.speed = speed
 
     def label(self):
-        return ['sigma',round(self.sigma,2),'empty', 'empty','empty','empty']
+        return ['sigma',round(self.sigma,2),'amplitude',round(self.amplitude,2), 'speed',round(self.speed,2),'empty','empty']
 
     def generate(self, step, dumpworld):
 
         world = np.zeros([3, 10, 10, 10])
 
-        x, y = np.mgrid[0:10:10j, 0:10:10j]
+        y, z = np.mgrid[0:10:10j, 0:10:10j]
 
         # Need an (N, 2) array of (x, y) pairs.
-        xy = np.column_stack([x.flat, y.flat])
+        yz = np.column_stack([y.flat, z.flat])
 
-        mu = np.array([4.5, 4.5])
+        mu = np.array([5, 5])
 
-        sigma = np.array([1.2, 1.2])
+        sigma = np.array([self.sigma,self.sigma])
         covariance = np.diag(sigma**2)
 
+        gauss = np.sin(step*self.speed)*multivariate_normal.pdf(yz, mean=mu, cov=covariance)*self.amplitude*self.sigma**2+5
+        gauss = gauss.reshape(10,10)
 
-        for x in range (10)
-            for y in range (10)
-                for z in range (10)
-                    gauss = multivariate_normal.pdf(xy, mean=mu, cov=covariance)*100+5
-                    # Reshape back to a (10, 10) grid.
-                    gauss = gauss.reshape(x.shape)
-                    world[:,x,y,z] = 1.0/(z-gauss)**8
+
+        for y in range (10):
+            for z in range (10):
+                for x in range (10):
+                    world[:,x,y,z] = np.exp(-abs(x-gauss[y,z])*5)
+
+        print
 
         return np.clip(world, 0, 1)
