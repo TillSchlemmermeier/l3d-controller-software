ad0383ef83e39e8b977f626bf463bdf9b815e8ff
Michael <co_eus@posteo.org>
2018-08-16 21:13:37 +0200

    small change in e_blur

--- effects/e_blur.py
+++ effects/e_blur.py
@@ -1,6 +1,7 @@
 # modules
 import numpy as np
 
+
 class e_blur():
     '''
     Effect: blur
@@ -10,23 +11,23 @@ class e_blur():
         self.amount = 0.1
 
     def control(self, amount, blub1, blub2):
-        self.amount = amount
+        self.amount = amount*0.01
 
     def label(self):
-        return ['Blur Amount', round(self.amount,2),
+        return ['Blur Amount', round(self.amount*10, 2),
                 'empty', 'empty',
                 'empty', 'empty']
 
 
     def generate(self, step, world):
 
-	world[0,:,:,:] = blur(world[0,:,:,:], self.amount) 
-	world[1,:,:,:] = blur(world[1,:,:,:], self.amount)
-	world[2,:,:,:] = blur(world[2,:,:,:], self.amount)
-		
+        world[0,:,:,:] = blur(world[0,:,:,:], self.amount)
+        world[1,:,:,:] = blur(world[1,:,:,:], self.amount)
+        world[2,:,:,:] = blur(world[2,:,:,:], self.amount)
+
         return np.clip(world, 0, 1)
-        
-        
+
+
 def blur(world, amount = 0.1):
     '''
     Effect: Blur
@@ -39,7 +40,7 @@ def blur(world, amount = 0.1):
     dim = 7
     a = gaussian(dim, amount+0.001, sym=True)
     gauss = world_init(dim)
-    center = dim/2
+    center = 4
 
     gauss[center-2:center+3, center-2:center+3, center-2:center+3] = a[0]
     gauss[center-1:center+2, center-1:center+2, center-1:center+2] = a[1]
