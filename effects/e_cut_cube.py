15163398807baa8107606908358d9ff04e9e11a2
Michael <co_eus@posteo.org>
2018-08-17 13:12:16 +0200

    added e_cut_cube

---
+++ effects/e_cut_cube.py
@@ -0,0 +1,41 @@
+# modules
+import numpy as np
+from random import randint
+
+class e_cut_cube():
+    '''
+    Effect: e_cut_cube
+    '''
+
+    def __init__(self):
+        self.speed = 2
+        
+    def control(self, speed, blub0, blub1):
+        self.speed = int(speed*10)
+
+    def label(self):
+        return ['speed',self.speed,'empty', 'empty','empty','empty']
+
+    def generate(self, step, world):
+
+        num = randint(0,7)        
+	
+	if step % self.speed = 0
+	    if num == 0:
+	        world[:, :5, :5, :5] = 0
+	    if num == 1:
+	        world[:, :5, :5, 5:] = 0
+	    if num == 2:
+	        world[:, :5, 5:, :5] = 0
+	    if num == 3:
+	        world[:, 5:, :5, :5] = 0
+	    if num == 4:
+	        world[:, :5, 5:, 5:] = 0
+	    if num == 5:
+	        world[:, 5:, :5, 5:] = 0
+	    if num == 6:
+	        world[:, 5:, 5:, :5] = 0
+	    if num == 7:
+	        world[:, 5:, 5:, 5:] = 0
+	        
+        return np.clip(world, 0, 1)
