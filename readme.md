f95f05a55bf60704d7fb58e5a1ee651fe013e32a
Michael <co_eus@posteo.org>
2018-12-29 17:53:21 +0100

    initial commit for rendering_engine

---
+++ readme.md
@@ -0,0 +1,12 @@
+# L3D Cube 3.0
+
+## Parameterset / Global Variable
+
+- 0 - 9: global parameters
+- 10 - 10: sound2light
+- 20 - 39: channel settings
+- 40 - 69: parameters for channel 1
+- 70 - 99: parameters for channel 2
+- 100 - 129: parameters for channel 3
+- 130 - 159: parameters for channel 4
+
---
+++ rendering_engine.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+
+class rendering_engine():
+    '''
+    L3D Cube 3.0
+    
+    class for the rendering engine
+    
+    assuming a global variable called
+    'global_parameter':
+    - numpy array with size of 255
+    - entries ordered as described in
+      readme
+      
+    '''
+    __init__(self):
+        # open connections, if possible
+        # otherwise write in textfile or so
+        
+        # initialise variables
+        self.framecounter = 
+        
+    run(self):
+        # check wether 'running' flag is set
+        if global.parameter[0] == 1:
+            # increase framecounter
+            self.framecounter += 1
+            # some test
+            print(self.framecounter)
+            
+            
+            
+            
