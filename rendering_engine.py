2410858c442b7c72c6588996065a4b810ec209e8
Michael <co_eus@posteo.org>
2018-12-30 11:14:14 +0100

    added connection to arduino, logging and rough draft for the other routines

--- rendering_engine.py
+++ rendering_engine.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import logging, serial
 
 class rendering_engine():
     '''
@@ -14,16 +15,54 @@ class rendering_engine():
       readme
       
     '''
-    def __init__(self):
-        # open connections, if possible
-        # otherwise write in textfile or so
+    def __init__(self, log = False):
         
         # initialise variables
         self.framecounter = 0
+        self.logging = log
+        self.debug = False
+        
+        self.header =   [int(66),
+                         int(69),
+                         int(69),
+                         int(70),
+                         int(1),	# Speed
+                         int(200),	# Brightness
+                         int(116),	# hPal
+                         int(0),	# hPalMode
+                         int(1),	# hRGBMode
+                         ]
+                
+        # take care of logging
+        if self.logging:
+            logging.basicConfig(filename='log_rendering_enginge.log',
+                                level=logging.DEBUG)
+        else:
+            logging.basicConfig(filename='log_rendering_enginge.log',
+                                level=logging.WARNING)
+        
+        logging.info('Variables initialised')
+        
+
+        # try to establish connection to arduino
+        try:
+            self.arduino = serial.Serial('/dev/ttyACM0', 230400)
+            logging.info('Connection to Arduino established')
+            logging.info(self.arduino)
+        except IOError:
+            self.arduino = serial.Serial('/dev/ttyACM1', 230400)
+            logging.info('Connection to Arduino established')
+            logging.info(self.arduino)
+	except:
+	    self.debug = True
+	    logging.warning('No Connection to Arduino established, entering DEBUG mode')
+
         
     def run(self):
         # check wether 'running' flag is set
         if global_parameter[0] == 1:
+            self.generate_frame()
+            self.send_frame()
             # increase framecounter
             self.framecounter += 1
             # some test to emulate output to arduino
@@ -31,4 +70,33 @@ class rendering_engine():
             
             
             
+    def send_frame(self):
+        '''
+        Function to convert world to voxel format,
+        and send it through serial interface to the
+        Arduino
+        '''
+        if not self.debug:
+            # assemble list
+            package = self.header + self.get_cubedata()
+            # send package 
+            self.arduino.write(bytearray(package))
+        else:
+            logging.info(self.get_cubedata())
             
+
+    def generate_frame(self):
+        '''
+        Calculates a new frame according to the
+        entries in the global parameter variable
+        
+        writes the result into self.cubeworld
+        '''
+        pass
+        
+    def get_cubedata(self):
+        '''
+        takes data from self.cubeworld and returns
+        voxel-style list
+        '''
+        pass
