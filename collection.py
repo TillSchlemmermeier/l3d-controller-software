a2569fe4ece6544dc9fdf3dbc16e0cc830401cce
Yannig <yannig@gmx.net>
2019-03-21 21:22:34 +0100

    gauss

--- collection.py
+++ collection.py
@@ -1,9 +1,10 @@
-
 from generators.g_blank import *
 from generators.g_random import *
 from generators.g_planes import *
+from generators.g_gauss import *
 from effects.e_blank import *
 from effects.e_rainbow import *
+
 
 
 generators = []
@@ -11,6 +12,7 @@ generators = []
 generators.append(g_blank)
 generators.append(g_random)
 generators.append(g_planes)
+generators.append(g_gauss)
 
 effects = []
 
