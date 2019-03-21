5f46891ff7e2257a8a43290deaaf7001f15df858
Yannig <yannig@gmx.net>
2019-03-21 21:33:41 +0100

    growing_sphere

--- collection.py
+++ collection.py
@@ -2,6 +2,7 @@ from generators.g_blank import *
 from generators.g_random import *
 from generators.g_planes import *
 from generators.g_gauss import *
+from generators.g_growing_sphere import *
 from effects.e_blank import *
 from effects.e_rainbow import *
 
@@ -13,6 +14,7 @@ generators.append(g_blank)
 generators.append(g_random)
 generators.append(g_planes)
 generators.append(g_gauss)
+generators.append(g_growing_sphere)
 
 effects = []
 
---
+++ generators/g_growing_sphere.py
@@ -0,0 +1,54 @@
+# modules
+import numpy as np
+from scipy.signal import sawtooth
+from generators.g_genhsphere import gen_hsphere
+
+# fortran routine is in g_growing_sphere_f.f90
+
+class g_growing_sphere():
+    '''
+    Generator: growing_sphere
+
+    a growing hollow sphere in the middle of the cube
+
+    Parameters:
+    - maxsize
+    - growspeed
+    - oscillate y/n
+    '''
+
+    def __init__(self):
+        self.maxsize = 10
+        self.growspeed = 1
+        self.oscillate = 0
+
+    #def label(self):
+    #    return ['maxsize',round(self.maxsize,2),'growspeed',round(self.growspeed,2),'osci, ex, im',round(self.oscillate,2)]
+    def return_values(self):
+        pass
+
+    #def control(self, maxsize, growspeed, oscillate):
+    def __call__(self, args):
+        self.maxsize = args[0]*10
+        self.growspeed = args[1]
+        self.oscillate = args[2]
+
+#def generate(self, step, dumpworld):
+        world = np.zeros([3, 10, 10, 10])
+
+        # oscillates between 0 and 1
+        if self.oscillate < 0.3:
+            osci = np.sin(step*self.growspeed)*0.5 + 1
+        elif self.oscillate > 0.7:
+            osci = sawtooth(step*self.growspeed, 0)*0.5 + 1
+        else:
+            osci = sawtooth(step*self.growspeed)*0.5 + 1
+
+        # scales to maxsize
+        size = self.maxsize * osci
+        # creates hollow sphere with parameters
+        world[0, :, :, :] = gen_hsphere(size, 4.5, 4.5, 4.5)
+        world[1:, :, :, :] = world[0, :, :, :]
+        world[2:, :, :, :] = world[0, :, :, :]
+
+        return np.round(np.clip(world, 0, 1), 3)
