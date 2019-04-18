106c01df42f8698d4a30de20344c1b49dcc90b55
Till_Schlemmermeier <till.schlemmermeier@gmail.com>
2019-04-18 11:53:54 +0200

    sound_color

--- CubeWorld.py
+++ CubeWorld.py
@@ -75,7 +75,7 @@ from effects.e_prod_saturation import e_prod_saturation
 from effects.e_prod_hue import e_prod_hue
 from effects.e_bright_osci import e_bright_osci
 from effects.e_cut_cube import e_cut_cube
-from effects.e_sound_color import e_sound_color
+#from effects.e_sound_color import e_sound_color
 
 #from effects.e_blur import e_blur
 from effects.e_rare_strobo import e_rare_strobo
