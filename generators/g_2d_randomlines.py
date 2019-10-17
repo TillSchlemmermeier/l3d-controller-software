5028c509c87e2eccf30e447c543a64daf8ed617d
Till_Schlemmermeier <till.schlemmermeier@gmail.com>
2019-10-17 18:35:42 +0200

    old from last subspace + updated 2d_randomlines

--- generators/g_2d_randomlines.py
+++ generators/g_2d_randomlines.py
@@ -16,19 +16,20 @@ class g_2d_randomlines():
 
     def generate(self, step, dumpworld):
 
-        world_2d = np.zeros([3, 40, 10])
+        world_2d = np.zeros([3, 10, 20])
         world = np.zeros([3, 10, 10, 10])
 
         direction = randint(0, 1)
         if direction == 0:
-            world[:, :, randint(0, 9)] = 1
+            world_2d[:, :, randint(0, 19)] = 1
         elif direction == 1:
-            world[:, randint(0, 39), :] = 1
+            world_2d[:, randint(0, 9), :] = 1
 
         # now we have to convert it
-        world[:, 0, :, :] = world_2d[:, :10, :]
-        world[:, 1, :, :] = world_2d[:, 10:20, :]
-        world[:, 2, :, :] = world_2d[:, 20:30, :]
-        world[:, 3, :, :] = world_2d[:, 30:40, :]
+#        print(np.shape(world[:, 0, :, :]), np.shape(world_2d[:, 10:20, :]))
+        world[:, :, :, 0] = world_2d[:, :, :10]
+        world[:, :, :, 1] = world_2d[:, :, 10:20]
+#        world[:, 2, :, :] = world_2d[:, 20:30, :]
+#        world[:, 3, :, :] = world_2d[:, 30:40, :]
 
         return np.clip(world, 0, 1)
