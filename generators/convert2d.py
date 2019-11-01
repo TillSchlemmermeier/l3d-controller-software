c3c10d70abd10d27681eae52913e94e25383daa9
Till_Schlemmermeier <till.schlemmermeier@gmail.com>
2019-11-01 20:33:12 +0100

    now really fixed convert2d

--- generators/convert2d.py
+++ generators/convert2d.py
@@ -8,6 +8,6 @@ def convert2d(world2d):
     world[:, :, :, 2] = world2d[:, 10:, 20:30]
     world[:, :, :, 3] = world2d[:, 10:, 30:40][:, ::-1, ::-1]
     world[:, :, :, 4] = world2d[:, :10, 10:20][:, ::-1, :]
-    world[:, :, :, 5] = world2d[:, :10, 20:30][:, ::-1, :]
+    world[:, :, :, 5] = world2d[:, :10, 20:30][:, ::-1, ::-1]
 
     return world
