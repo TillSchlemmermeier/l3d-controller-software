24100c06c7c255eb3c8795d814303b8913f7e049
Michael <co_eus@posteo.org>
2020-05-06 11:09:05 +0200

    added first version of e_zoom and g_grow

---
+++ effects/e_zoom.py
@@ -0,0 +1,28 @@
+# modules
+import numpy as np
+from scipy.ndimage import zoom
+
+class e_zoom():
+    '''Effect: zoom'''
+
+    def __init__(self):
+        self.amount = 0.5
+        self.degree = 1
+
+    def return_values(self):
+        return [['', ''],
+				['', ''],
+				['', '']]
+
+    def __call__(self, world, args):
+		# parse input
+        self.amount = args[0]
+        self.degree = int(args[1]*3+1)
+
+        zoomworld = np.zeros([3, 10, 10, 10])
+
+        for i in range(self.degree):
+            for c in range(3):
+                zoomworld[c, :, :, :] += zoom(world[c, :, :; :], i, mode = 'constant')[5:-5, 5:-5, 5:-5]
+
+        return np.clip(world+self.amount*zoomworld, 0, 1)
