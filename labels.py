e1499fc5d78a64df1c1a923ac115d2a69ecd42dc
Yannig <yannig@gmx.net>
2020-05-23 21:10:02 +0200

    added torus to collection

--- labels.py
+++ labels.py
@@ -71,7 +71,7 @@ for i in [2, 7, 12, 17]:
     labels[i, 5, 1] = 'a_multi\ncube_edges'
     labels[i, 5, 2] = 'a_pulsating'
     labels[i, 5, 3] = 'a_orbot'
-    labels[i, 5, 4] = 'empty'
+    labels[i, 5, 4] = 'torus'
     labels[i, 5, 5] = 'empty'
     labels[i, 5, 6] = 'empty'
     labels[i, 5, 7] = 'empty'
