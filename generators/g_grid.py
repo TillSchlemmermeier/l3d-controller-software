d09ce6517278fd34be4a85377b39cbc0e8716fde
Yannig <yannig@gmx.net>
2020-05-30 19:36:25 +0200

    g_grid

---
+++ generators/g_grid.py
@@ -0,0 +1,36 @@
+# modules
+import numpy as np
+
+
+class g_grid():
+    '''
+    Generator: grid
+    a 3D grid
+    Parameters:
+    - step
+    '''
+
+    def __init__(self):
+        self.stepX = 2
+        self.stepY = 2
+        self.stepZ = 2
+
+    #Strings for GUI
+    def return_values(self):
+        return [b'grid', b'stepX', b'stepY', b'stepZ', b'']
+
+    def __call__(self, args):
+        self.stepX = round(args[0]*9)
+        self.stepY = round(args[0]*9)
+        self.stepZ = round(args[0]*9)
+
+        # create world
+        world = np.zeros([3, 10, 10, 10])
+
+        for x in range(9):
+            for y in range(9):
+                for z in range(9):
+                    if x%self.stepX == 0 and y%self.stepY == 0 and z%self.stepZ == 0:
+                        world[:, x, y, z] = 1.0
+
+        return np.clip(world, 0, 1)
