43fd6448b168c51a76e95d84e4c56b802b8ebdef
Michael <co_eus@posteo.org>
2020-06-07 22:20:46 +0200

    added fade to e_mean

--- effects/e_mean.py
+++ effects/e_mean.py
@@ -8,17 +8,22 @@ class e_mean():
         self.amount = 0.1
         self.mean = np.zeros([3,3,3])
         self.mean[:, :, :] = 1/18.0
+        self.fade = 0.1
 
-    #strings for GUI
+        self.fadeworld = np.zeros([3, 10, 10, 10])
+
     def return_values(self):
-        return [b'mean', b'amount', b'', b'', b'']
-
+        return [b'mean', b'amount', b'fade', b'', b'']
 
     def __call__(self, world, args):
         # parsing input
         self.amount = args[0]
+        self.fade = args[1]
 
         for i in range(3):
+            world[i, :, :, :] = (1-self.fade)*world[i, :, :, :] + self.fade*self.fadeworld[i, :, :, :]
             world[i, :, :, :] = (1-self.amount)*world[i, :, :, :] + self.amount*fftconvolve(world[i, :, :, :], self.mean, mode='same')
+            self.fadeworld[i, :, :, :] = np.clip(self.world[i, :, :, :], 0, 1)
+
 
         return np.clip(world, 0, 1)
