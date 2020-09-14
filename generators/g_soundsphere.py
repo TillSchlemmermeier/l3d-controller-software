eb014fa0fa683af18e50f77c525a25f00790d50e
Yannig <yannig@gmx.net>
2020-09-14 22:54:20 +0200

    soundsphere with new s2l routine

--- generators/g_soundsphere.py
+++ generators/g_soundsphere.py
@@ -6,30 +6,11 @@ import scipy
 import struct
 from time import sleep
 from generators.g_genhsphere import gen_hsphere
+from multiprocessing import shared_memory
 
 
 class g_soundsphere():
     def __init__(self):
-        # initialize pyaudio
-        self.sample_rate = 44100
-        self.buffer_size = int(44100/20)
-        p = pyaudio.PyAudio()
-
-        self.stream = p.open(
-            format = pyaudio.paInt16,	# paFloat32 vs paInt16 ?
-            channels = 1,
-            rate = self.sample_rate,
-            input = True,
-            output = False,
-            frames_per_buffer = self.buffer_size,
-#            timeout = 0.001
-            )
-
-        # parameters for normalization
-        self.buffer = []
-        self.normalized = False
-        self.norm = [0, 1]
-        self.norm_trigger_value = 0
 
         # parameters
         self.amount = 1.0
@@ -38,14 +19,15 @@ class g_soundsphere():
         self.smooth = 0.25
 
         self.last_value = 0
+        self.sound_values = shared_memory.SharedMemory(name = "global_s2l_memory")
 
     def return_values(self):
         # strings for GUI
-        return [b'SoundSphere', b'maxsize', b'channel', b'smooth', b'normalize switch']
+        return [b'SoundSphere', b'maxsize', b'channel', b'smooth', b'']
 
 
     def return_gui_values(self):
-        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.maxsize,2)), str(round(self.channel,2)), str(round(self.smooth,2)), str(round(self.norm_trigger_value,2))),'utf-8')
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.maxsize,2)), str(round(self.channel,2)), str(round(self.smooth,2)), '','utf-8')
 
 
     def __call__(self, args):
@@ -54,38 +36,10 @@ class g_soundsphere():
 
         # process parameters
         self.maxsize = args[0]*10
+        self.channel = int(args[1]*3)
         self.smooth = args[2] * 0.5
-        self.channel = int(args[1]*len(total_volume)-1)
 
-        # detect change at normalize parameter or channel
-        if self.norm_trigger_value != args[3]:
-            print('clearing buffer')
-            # clear buffer and reset normalized switch
-            self.buffer = []
-            self.normalized = False
-            self.norm_trigger_value = args[3]
-
-
-        if not self.normalized :
-            # fill buffer
-            self.buffer.append(total_volume[self.channel])
-            # self.buffer.append(total_volume[self.channel])
-
-            # save min and max
-            self.norm[0] = min(self.buffer)
-            self.norm[1] = max(self.buffer)
-
-            if len(self.buffer) > 50:
-                self.normalized = True
-                print('normalized:', self.norm)
-
-        # apply normalization
-        if self.norm[1] > self.norm[0]:
-            # the ()**4 is important! otherwise, this is just bright or dark
-            current_volume = ((total_volume[self.channel]-self.norm[0]) /(self.norm[1]-self.norm[0]))**4
-            #print(current_volume, total_volume[self.channel], self.norm[0], self.norm[1])
-        else:
-            current_volume = total_volume[self.channel]
+        current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))
 
         # mix with old value
         current_volume = (1-self.smooth) * current_volume + self.smooth * self.last_value
@@ -99,59 +53,3 @@ class g_soundsphere():
         world[2:, :, :, :] = world[0, :, :, :]
 
         return np.clip(world, 0, 1)
-
-
-    def get_fft(self, data):
-        FFT = fft(data)
-        # Returns an array of complex numbers
-        freqs = fftfreq(self.buffer_size, 1.0/self.sample_rate)
-
-        # Get amplitude and scale
-        y = abs(FFT[0:int(len(FFT)/2)])/1000
-        y = scipy.log(y) - 2
-        # Subtract noise floor (empirically determined)
-        return (freqs, y)
-
-
-    def update_line(self):
-
-        temp_buf = self.stream.read(100)
-
-        while True:
-            # read buffer and calculate spectrum
-            temp_buf += self.stream.read(self.stream.get_read_available())
-            if len(temp_buf) > self.buffer_size*2:
-                break
-
-        buf = temp_buf[-self.buffer_size*2:]
-#        print(self.buffer_size, len(temp_buf), len(buf))
-
-        data = scipy.array(struct.unpack("%dh"%(self.buffer_size), buf))
-
-        freqs, y = self.get_fft(data)
-
-        # print(len(buf))
-
-        # Average into chunks of N
-        #N = 10
-        #yy = [scipy.average(y[n:n+N]) for n in range(0, len(y), N)]
-        #yy = yy[:int(len(yy)/2)] # Discard half of the samples, as they are mirrored
-
-        yy = y
-        #print(yy)
-
-        # now do some threshold detection
-        # for i in range(len(yy)):
-        #     yy[i] = self.control_threshold(yy[i], self.threshold)
-
-        return np.round(yy, 2)
-
-
-'''
-    def control_threshold(self, dat, thres):
-        if dat < thres:
-            return 0.0
-        else:
-            return dat
-
-'''
