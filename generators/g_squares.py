95bbac6879e1216573f714019d86984f081a1ecd
Yannig <yannig@gmx.net>
2020-10-24 19:42:44 +0200

    repair squares

--- generators/g_squares.py
+++ generators/g_squares.py
@@ -70,7 +70,7 @@ class g_squares():
         # check if S2L is activated
         if self.channel >= 0:
             current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))
-            if current_volume > 0
+            if current_volume > 0:
                 self.step += 1
         else:
             self.step += 1
