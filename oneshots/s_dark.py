627b3be4e56d6932b4c53043a7d25bb9cbaf1636
Michael <co_eus@posteo.org>
2020-10-24 21:10:41 +0200

    added more oneshots

---
+++ oneshots/s_dark.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class s_dark:
+
+    def __init__(self):
+        self.counter = 1
+        self.lastworld = np.zeros([3, 10, 10, 10])
+
+    def __call__(self, world):
+
+        if self.counter > 0:
+            world *= (1-self.counter)**4
+            self.counter -= 0.05
+
+        return world, self.counter
