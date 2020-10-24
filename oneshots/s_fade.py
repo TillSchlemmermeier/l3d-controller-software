fd386153c9a26811d1071de1a48221641ca28f85
Michael <co_eus@posteo.org>
2020-10-24 20:50:36 +0200

    fixed s_fade

--- oneshots/s_fade.py
+++ oneshots/s_fade.py
@@ -3,27 +3,15 @@ import numpy as np
 class s_fade:
 
     def __init__():
-        self.counter = 6
+        self.counter = 1
         self.lastworld = np.zeros([3, 10, 10, 10])
 
     def __call__(world):
-        if self.counter == 6:
-            world[:, :, :, 0] = 1.0
-            self.counter -= 1
-        elif self.counter == 5:
-            world[:, 0, :, :] = 1.0
-            self.counter -= 1
-        elif self.counter == 4:
-            world[:, :, 9, :] = 1.0
-            self.counter -= 1
-        elif self.counter == 3:
-            world[:, :, 0, :] = 1.0
-            self.counter -= 1
-        elif self.counter == 2:
-            world[:, 9, :, :] = 1.0
-            self.counter -= 1
-        elif self.counter == 1:
-            world[:, :, :, 9] = 1.0
-            self.counter -= 1
 
+        if self.counter > 0:
+            world += self.lastworld*(1-self.counter)
+            self.counter -= 0.1
+
+            self.lastworld[:, :, :, :] = world[:, :, :, :]
+            
         return world, self.counter
