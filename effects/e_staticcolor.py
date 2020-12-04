da8f35e041bf06904e4c315465835f5aa585b439
Yannig <yannig@gmx.net>
2020-12-04 20:45:33 +0100

    staticcolor with HSV

--- effects/e_staticcolor.py
+++ effects/e_staticcolor.py
@@ -1,5 +1,6 @@
 # modules
 import numpy as np
+from colorsys import rgb_to_hsv, hsv_to_rgb
 
 class e_staticcolor():
     '''
@@ -13,22 +14,30 @@ class e_staticcolor():
         self.red = 1.0
         self.green = 1.0
         self.blue = 1.0
+        self.hsv = [0.0,0.0,0.0]
 
     #strings for GUI
     def return_values(self):
-        return [b'staticcolor', b'Red', b'Green', b'Blue', b'']
+        return [b'staticcolor', b'Red', b'Green', b'Blue', b'HSV']
 
     def return_gui_values(self):
-        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.red,1)), str(round(self.green,1)), str(round(self.blue,1)), ''), 'utf-8')
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.red,1)), str(round(self.green,1)), str(round(self.blue,1)), str(round(self.hsv[0],1))), 'utf-8')
 
     def __call__(self, world, args):
 
         self.red   = args[0]
         self.green = args[1]
         self.blue  = args[2]
+        self.hsv[0]= args[3]
 
-        world[0, :, :, :] = world[0, :, :, :]*self.red
-        world[1, :, :, :] = world[1, :, :, :]*self.green
-        world[2, :, :, :] = world[2, :, :, :]*self.blue
+        if self.hsv[0] > 0:
+            self.hsv = hsv_to_rgb(self.hsv, 1, 1)
+            self.red   = self.hsv[0]
+            self.green = self.hsv[1]
+            self.blue  = self.hsv[2]
+
+        world[0, :, :, :] *= self.red
+        world[1, :, :, :] *= self.green
+        world[2, :, :, :] *= self.blue
 
         return np.clip(world, 0, 1)
