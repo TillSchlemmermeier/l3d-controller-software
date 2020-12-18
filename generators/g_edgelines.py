9ba142df4962c3e47e7e2d81f0e995ce3ebc88bf
Yannig <yannig@gmx.net>
2020-12-18 23:54:01 +0100

    s2l edgelines

--- generators/g_edgelines.py
+++ generators/g_edgelines.py
@@ -72,7 +72,7 @@ class g_edgelines():
         # check if S2L is activated
         if self.channel >= 0:
             current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))
-            if current_volume > 0:
+            if current_volume <= 0:
                 self.counter = (self.counter % 10) + 1
                 self.counter *= 10 
 
