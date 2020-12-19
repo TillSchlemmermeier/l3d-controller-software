a6af2f851e6687102c79eb000e212b7a0dc166d2
Yannig <yannig@gmx.net>
2020-12-19 02:02:17 +0100

    s2l snake

--- generators/g_snake.py
+++ generators/g_snake.py
@@ -1,6 +1,6 @@
 import numpy as np
 from random import randint, uniform, choice
-
+from multiprocessing import shared_memory
 
 class g_snake():
     '''
@@ -24,19 +24,40 @@ class g_snake():
         self.world = np.zeros([3, 10, 10, 10])
 #        self.world[:, randint(0, 9), randint(0, 9), randint(0, 9)] = 1.0
         self.world[:, 4, 4, 4] = 1.0
+        #s2l
+        self.sound_values = shared_memory.SharedMemory(name = "global_s2l_memory")
+        self.channel = 0
 
     def return_values(self):
-        return [b'snake', b'turn', b'', b'', b'']
+        return [b'snake', b'turn', b'', b'', b'channel']
 
 
     def return_gui_values(self):
-        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.turnprop,2)), '', '', ''),'utf-8')
+        if self.channel >=0:
+            channel = str(self.channel)
+        else:
+            channel = 'noS2L'
+
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.turnprop,2)), '', '', channel),'utf-8')
 
 
     def __call__(self, args):
         self.turnprop = 1-args[0]
+        self.channel = int(args[3]*4)-1
 
         world = np.zeros([3,10,10,10])
+
+        # check if S2L is activated
+        if self.channel >= 0:
+            current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))
+            self.turnprop = 1
+            if current_volume > 0:
+                #choose direction
+                oldaxis = self.axis
+                while oldaxis == self.axis:
+                    self.axis = randint(1, 3)
+
+                self.direction = choice([-1, 1])
 
         # choose direction
         if uniform(0, 1) > self.turnprop:
