3126f4ab6c3508a9449ee7efd44654a86950171b
Yannig <yannig@gmx.net>
2020-12-19 02:19:35 +0100

    s2l swell

--- generators/g_swell.py
+++ generators/g_swell.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.signal import sawtooth
 from generators.g_shooting_star_f import gen_shooting_star
 from random import randint
+from multiprocessing import shared_memory
 
 class g_swell():
 
@@ -13,27 +14,40 @@ class g_swell():
         self.counter = 0
         self.brightness = 0.0
         self.dir = 1.0
+        #s2l
+        self.sound_values = shared_memory.SharedMemory(name = "global_s2l_memory")
+        self.channel = 0
 
-    def control(self, speed, theta, rho):
-        self.speed = speed+0.05
 
     #Strings for GUI
     def return_values(self):
-        return [b'swell', b'speed', b'', b'', b'']
+        return [b'swell', b'speed', b'', b'', b'channel']
 
     def return_gui_values(self):
-        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.speed,2)), '', '', ''),'utf-8')
+        if self.channel >=0:
+            channel = str(self.channel)
+        else:
+            channel = 'noS2L'
+
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.speed,2)), '', '', channel),'utf-8')
 
 
     def __call__(self, args):
 
         # parse parameters
         self.speed = args[0]+0.05
+        self.channel = int(args[3]*4)-1
 
         # generate empty world
         world1 = np.zeros([10, 10, 10])
         world2 = np.zeros([10, 10, 10])
         world = np.zeros([3, 10, 10, 10])
+
+        # check if S2L is activated
+        if self.channel >= 0:
+            current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))
+            if current_volume > 0:
+                self.speed = current_volume
 
         # create new point
         if self.brightness <= 0.0:
