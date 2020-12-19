50e8c13ddd925a65f2c45f60d040ea1c2f226206
Yannig <yannig@gmx.net>
2020-12-19 01:20:27 +0100

    color correction rising square

--- generators/g_rising_square.py
+++ generators/g_rising_square.py
@@ -1,6 +1,7 @@
 # modules
 import numpy as np
 from random import randint, uniform
+from colorsys import hsv_to_rgb
 
 class g_rising_square():
     '''
@@ -48,7 +49,8 @@ class g_rising_square():
                     self.flatworld[:, :, 9, :] = 1.0
             else:
                 for i in range(self.nled):
-                    color = color_translate(uniform(0,127))
+                    color = hsv_to_rgb(uniform(0, 1), 1, 1)
+
                     self.flatworld[0, :, 9, :] = color[0]
                     self.flatworld[1, :, 9, :] = color[1]
                     self.flatworld[2, :, 9, :] = color[2]
@@ -64,41 +66,3 @@ class g_rising_square():
             self.flatworld[:, :, 9, :] = 0.0
         self.step += 1
         return np.clip(world, 0, 1)
-
-
-def color_translate(value):
-    #value *= 127
-    #translates values from 0 to 127 to rgb values
-    #if value>127:
-    #    value-=127
-    value = value % 127
-    r_out = 0.0
-    g_out = 0.0
-    b_out = 0.0
-
-    if(value<=21):
-        r_out=1
-        g_out=value/21.0
-        b_out=0
-    elif(value>21 and value<=42):
-        r_out=1-((value-21.0)/21.0)
-        g_out=1
-        b_out=0
-    elif(value>42 and value<=63):
-        r_out=0
-        g_out=1
-        b_out=(value-42.0)/21.0
-    elif(value>63 and value<=84):
-        r_out=0
-        g_out=1-((value-63.0)/21.0)
-        b_out=1
-    elif(value>84 and value<=105):
-        r_out=(value-84.0)/21.0
-        g_out=0
-        b_out=1
-    elif(value>105 and value <=127):
-        r_out=1
-        g_out=0
-        b_out=1-((value-105.0)/21.0)
-
-    return [r_out, g_out, b_out]
