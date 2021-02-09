c6bd8f4ded246dc32d913ce010c4911b787220f4
unknown <yannig@gmx.net>
2021-02-09 23:30:21 +0100

    added s2l trigger

--- effects/e_s2l.py
+++ effects/e_s2l.py
@@ -1,10 +1,5 @@
 
 import numpy as np
-import pyaudio
-from scipy.fftpack import fft, fftfreq
-import scipy
-import struct
-from time import sleep
 from multiprocessing import shared_memory
 
 class e_s2l():
@@ -15,22 +10,45 @@ class e_s2l():
         self.amount = 1.0
         self.channel = 1.0
         self.sound_values = shared_memory.SharedMemory(name = "global_s2l_memory")
+        self.lastvalue = 0
+        self.counter = 0
+        self.step = 8
 
     def return_values(self):
         # strings for GUI
         return [b's2l', b'amount', b'channel', b'', b'']
 
     def return_gui_values(self):
-        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.amount,1)), str(self.channel), '','') ,'utf-8')
+        if self.channel < 4:
+            channel = str(self.channel)
+        else:
+            channel = "Trigger"
+
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.amount,1)), channel, '','') ,'utf-8')
 
     def __call__(self, world, args):
         # process parameters
         self.amount = args[0]
-        self.channel = int(args[1]*3)
-        current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))**4
+        self.channel = int(args[1]*4)
 
         # apply manipulation
-        for i in range(3):
-            world[i, :, :, :] *= (1-self.amount) + np.clip(current_volume,0,1)*self.amount
+        if self.channel < 4:
+            current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))**4
+            #for i in range(3):
+            #    world[i, :, :, :] *= (1-self.amount) + np.clip(current_volume,0,1)*self.amount
+
+            # maybe edit formula?
+            world[:, :, :, :] *= (1-self.amount) + np.clip(current_volume,0,1)*self.amount
+
+        else:
+            current_volume = int(float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8')))
+            if current_volume > self.lastvalue:
+                self.counter = 0
+
+            # maybe more than 8:
+            if self.counter < self.step:
+                world[:, :, :, :] *= (1 - self.amount) + ((self.step - self.counter) / self.step) * self.amount
+
+            self.counter += 1
 
         return np.clip(world, 0, 1)
