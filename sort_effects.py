7911b619875664b66ceba118fa1cc289413e1093
unknown <yannig@gmx.net>
2021-03-10 16:24:06 +0100

    sort effects and generators

--- sort_effects.py
+++ sort_effects.py
@@ -24,6 +24,57 @@ def insert_eff(effects, new_effect, pos):
     temp.insert(pos, new_effect+'\n')
     return temp
 
+
+def remove_eff(effects, remove_effect):
+    effect_used = False
+    temp = deepcopy(effects)
+    index = temp.index(remove_effect + '\n')
+
+    with open('presets.dat', 'r') as file:
+        presets = file.readlines()
+
+    list_presets = []
+    for p in presets:
+        list_presets.append(p.strip('\n').split())
+
+    for preset in list_presets:
+        # loop over effects
+        for j in range(2, 5):
+            if int(float(preset[j])) == index:
+                effect_used = True
+                print("effect used in preset " + preset[0] + ". Edit or remove preset first")
+
+
+    with open('global_presets.dat', 'r') as file:
+        presets = file.readlines()
+
+    list_presets = []
+    for p in presets:
+        list_presets.append(p.strip('\n').split())
+
+    for preset in list_presets:
+        # loop over channels
+        for i in range(4):
+            # loop over effects
+            for j in range(2, 5):
+                if int(float(preset[5*i+j])) == index:
+                    effect_used = True
+                    print("effect used in global preset " + preset[0] + ". Edit or remove global preset first")
+
+        # loop over global effects
+        for i in range(3):
+            if int(float(preset[140+i])) == index:
+                effect_used = True
+                print("effect used in global preset " + preset[0] + " as global effect. Edit or remove global preset first")
+
+
+    if not effect_used:
+        temp.remove(remove_effect + '\n')
+        print("effect " + remove_effect + " removed")
+
+    return temp
+
+
 def get_dictionary(old_effs, new_effs):
 
     swap_dict = {}
@@ -31,8 +82,11 @@ def get_dictionary(old_effs, new_effs):
     new_effs = np.array(new_effs)
 
     for i in range(len(old_effs)):
-        ind = int(np.where(old_effs[i] == new_effs)[0][0])
-        swap_dict[i] = ind
+        try:
+            ind = int(np.where(old_effs[i] == new_effs)[0][0])
+            swap_dict[i] = ind
+        except:
+            print("making dictionary, effect " + str(i) + " now missing")
 
     return swap_dict
 
@@ -47,22 +101,16 @@ def update_presets(swap_dict):
 
     # change number
     for preset in list_presets:
-        for i in range(4):
-            for j in range(2, 5):
-                ind = int(float(preset[5*i+j]))
-                preset[5*i+j] = str(swap_dict[ind])
-                '''
-                2 3 4
-                7 8 9
-                12 13 14
-                '''
+        for j in range(2, 5):
+            ind = int(float(preset[j]))
+            preset[j] = str(swap_dict[ind])
 
-    print('trying to save file...')
+    print('trying to save presets...')
     # saving modified presets
     with open('presets.dat', 'w') as file:
         for preset in list_presets:
             file.write(' '.join(preset) + '\n')
-
+    print("presets successfully saved")
 
 def update_global_presets(swap_dict):
     # loading and parsing presets
@@ -77,44 +125,53 @@ def update_global_presets(swap_dict):
     for preset in list_presets:
         # loop over channels
         for i in range(4):
+            # loop over effects
             for j in range(2, 5):
                 ind = int(float(preset[5*i+j]))
-                #print(5*i, j, ind)
                 preset[5*i+j] = str(swap_dict[ind])
 
+        # loop over global effects
         for i in range(3):
             ind = int(float(preset[140+i]))
             preset[140+i] = str(swap_dict[ind])
 
+    print('trying to save global presets...')
     # saving modified presets
     with open('global_presets.dat', 'w') as file:
         for preset in list_presets:
             file.write(' '.join(preset) + '\n')
+    print("global presets successfully saved")
 
 
 if __name__ == "__main__":
     print(sys.argv)
 
-    with open('effects.dat', 'r') as file:
-        effects = file.readlines()
+    try:
+        with open('effects.dat', 'r') as file:
+            effects = file.readlines()
 
-    if sys.argv[1] == 'a':
-        new_effects = append_eff(effects, sys.argv[2])
-    elif sys.argv[1] == 'i':
-        new_effects = insert_eff(effects, sys.argv[2], int(sys.argv[3]))
-    elif sys.argv[1] == 's':
-        new_effects = swap_eff(effects, sys.argv[2], sys.argv[3])
+        if sys.argv[1] == 'a':
+            new_effects = append_eff(effects, sys.argv[2])
+        elif sys.argv[1] == 'i':
+            new_effects = insert_eff(effects, sys.argv[2], int(sys.argv[3]))
+        elif sys.argv[1] == 's':
+            new_effects = swap_eff(effects, sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == 'r':
+            new_effects = remove_eff(effects, sys.argv[2])
 
+        if effects != new_effects:
+            swap_dict = get_dictionary(effects, new_effects)
+            update_presets(swap_dict)
+            update_global_presets(swap_dict)
 
-    swap_dict = get_dictionary(effects, new_effects)
-    update_presets(swap_dict)
-    update_global_presets(swap_dict)
+            with open('effects.dat', 'w') as file:
+                for line in new_effects:
+                    file.write(line)
+            print("effects.dat updated")
 
-    with open('effects.dat', 'w') as file:
-        for line in new_effects:
-            file.write(line)
-'''
+        else:
+            print("no changes saved")
+
     except:
         print('ERROR!' )
         print('usage: python3.8 sort_effects.py <a/i/s> <new_effect/new_effect/effect 1> <NaN/pos/effect 2>')
-'''
--- sort_generators.py
+++ sort_generators.py
@@ -24,6 +24,46 @@ def insert_gen(generators, new_generator, pos):
     temp.insert(pos, new_generator+'\n')
     return temp
 
+def remove_gen(generators, remove_generator):
+    generator_used = False
+    temp = deepcopy(generators)
+    index = temp.index(remove_generator + '\n')
+
+    with open('presets.dat', 'r') as file:
+        presets = file.readlines()
+
+    list_presets = []
+    for p in presets:
+        list_presets.append(p.strip('\n').split())
+
+    for preset in list_presets:
+        if int(float(preset[1])) == index:
+            generator_used = True
+            print("generator used in preset " + preset[0] + ". Edit or remove preset first")
+
+
+    with open('global_presets.dat', 'r') as file:
+        presets = file.readlines()
+
+    list_presets = []
+    for p in presets:
+        list_presets.append(p.strip('\n').split())
+
+    for preset in list_presets:
+        # loop over channels
+        for i in range(4):
+            if preset[1+5*i] == index:
+                generator_used = True
+                print("generator used in global preset " + preset[0] + ". Eedit or remove global preset first")
+
+
+    if not generator_used:
+        temp.remove(remove_generator+'\n')
+        print("generator " + remove_generator + " removed")
+
+    return temp
+
+
 def get_dictionary(old_gens, new_gens):
 
     swap_dict = {}
@@ -31,8 +71,11 @@ def get_dictionary(old_gens, new_gens):
     new_gens = np.array(new_gens)
 
     for i in range(len(old_gens)):
-        ind = int(np.where(old_gens[i] == new_gens)[0][0])
-        swap_dict[i] = ind
+        try:
+            ind = int(np.where(old_gens[i] == new_gens)[0][0])
+            swap_dict[i] = ind
+        except:
+            print("making dictionary, generator " + str(i) + " now missing")
 
     return swap_dict
 
@@ -50,12 +93,12 @@ def update_presets(swap_dict):
         ind = int(float(preset[1]))
         preset[1] = str(swap_dict[ind])
 
-    print('trying to save file...')
+    print('trying to save presets...')
     # saving modified presets
     with open('presets.dat', 'w') as file:
         for preset in list_presets:
             file.write(' '.join(preset) + '\n')
-
+    print("presets successfully saved")
 
 def update_global_presets(swap_dict):
     # loading and parsing presets
@@ -68,19 +111,17 @@ def update_global_presets(swap_dict):
 
     # change number
     for preset in list_presets:
-        ind = int(float(preset[1]))
-        preset[1] = str(swap_dict[ind])
-        ind = int(float(preset[6]))
-        preset[6] = str(swap_dict[ind])
-        ind = int(float(preset[11]))
-        preset[11] = str(swap_dict[ind])
-        ind = int(float(preset[16]))
-        preset[16] = str(swap_dict[ind])
+        # loop over channels
+        for i in range (4):
+            ind = int(float(preset[1+i*5]))
+            preset[1+i*5] = str(swap_dict[ind])
 
+    print('trying to save global presets...')
     # saving modified presets
     with open('global_presets.dat', 'w') as file:
         for preset in list_presets:
             file.write(' '.join(preset) + '\n')
+    print("global presets successfully saved")
 
 
 if __name__ == "__main__":
@@ -96,15 +137,23 @@ if __name__ == "__main__":
             new_generators = insert_gen(generators, sys.argv[2], int(sys.argv[3]))
         elif sys.argv[1] == 's':
             new_generators = swap_gen(generators, sys.argv[2], sys.argv[3])
+        elif sys.argv[1] == 'r':
+            new_generators = remove_gen(generators, sys.argv[2])
 
 
-        swap_dict = get_dictionary(generators, new_generators)
-        update_presets(swap_dict)
-        update_global_presets(swap_dict)
+        if generators != new_generators:
+            swap_dict = get_dictionary(generators, new_generators)
+            update_presets(swap_dict)
+            update_global_presets(swap_dict)
 
-        with open('generators.dat', 'w') as file:
-            for line in new_generators:
-                file.write(line)
+            with open('generators.dat', 'w') as file:
+                for line in new_generators:
+                    file.write(line)
+            print("generators.dat updated")
+
+        else:
+            print("no changes saved")
+
     except:
         print('ERROR!' )
-        print('usage: python3.8 sort_generators.py <a/i/s> <new_generator/new_generator/generator 1> <NaN/pos/generator 2>')
+        print('usage: python3.8 sort_generators.py <a/i/s/r> <new_generator/new_generator/generator 1/remove_generator> <NaN/pos/generator 2>')
