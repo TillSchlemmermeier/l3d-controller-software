d07e0b72096830ee98a22efe4e53f1121f28a311
yannig@gmx.net <yannig@gmx.net>
2021-03-16 00:34:05 +0100

    repaired g_squared

--- generators/g_squares.py
+++ generators/g_squares.py
@@ -19,22 +19,24 @@ class g_squares():
         self.dir = 1
         self.type = 0
         self.step = 0
-
+        self.position = 0
+        # s2l
         self.sound_values = shared_memory.SharedMemory(name = "global_s2l_memory")
-        self.channel = 0
+        self.trigger = False
         self.lastvalue = 0
+        self.nextposition = 0
+        self.switch = False
+        self.stop = False
 
     #Strings for GUI
     def return_values(self):
-        return [b'squares', b'speed', b'dir', b'type', b'channel']
+        return [b'squares', b'speed', b'dir', b'type', b'Trigger']
 
     def return_gui_values(self):
-        if 4 > self.channel >= 0:
-            channel = str(self.channel)
-        elif self.channel == 4:
-            channel = "Trigger"
+        if self.trigger:
+            trigger = 'On'
         else:
-            channel = 'noS2L'
+            trigger = 'Off'
 
         if self.dir == 0:
             dir = 'X'
@@ -44,13 +46,13 @@ class g_squares():
             dir = 'Z'
 
         if self.type < 0.33:
-            type = 'sin'
+            type = 'cos'
         elif self.type >= 0.33 and self.type < 0.66:
             type = 'up'
         else:
             type = 'down'
 
-        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.speed,2)), dir, type, channel),'utf-8')
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.speed,2)), dir, type, trigger),'utf-8')
 
 
     #def generate(self, step, dumpworld):
@@ -58,55 +60,76 @@ class g_squares():
         self.speed = int(args[0]*9)
         self.dir = int(round(args[1]*3))
         self.type = args[2]
-        self.channel = int(args[3]*5)-1
 
-    #def generate(self, step, dumpworld):
+        if args[3] < 0.5:
+            self.trigger = False
+        else:
+            self.trigger = True
+
+        #def generate(self, step, dumpworld):
         world = np.zeros([3, 10, 10, 10])
-        if self.type < 0.33:
-            position = int( round((np.sin(0.1*self.step*self.speed)+1)*4.5))
-        elif self.type >= 0.33 and self.type < 0.66:
-            position = int( round((sawtooth(0.1*self.step*self.speed)+1)*4.5))
-        else:
-            position = int( round((sawtooth(0.1*self.step*self.speed, width=0)+1)*4.5))
-
-        # check if S2L is activated
-        if 4 > self.channel >= 0:
-            current_volume = float(str(self.sound_values.buf[self.channel*8:self.channel*8+8],'utf-8'))
-            current_volume = int(current_volume*6)
-
-            if self.dir == 0:
-                world[:, position-current_volume:position,4-current_volume:4+current_volume,4-current_volume:4+current_volume] = 1.0
-                world[:, :, 1:-1, 1:-1] = 0.0
-            elif self.dir == 1:
-                world[:, 4-current_volume:current_volume, position-current_volume:position,4-current_volume:4+current_volume] = 1.0
-                world[:, 1:-1, :, 1:-1] = 0.0
-            else:
-                world[:, 4-current_volume:4+current_volume,4-current_volume:4+current_volume,position-current_volume:position] = 1.0
-                world[:, 1:-1, 1:-1, :] = 0.0
-
-        else:
-            if self.dir == 0:
-                world[:, position,:,:] = 1.0
-                world[:, :, 1:-1, 1:-1] = 0.0
-            elif self.dir == 1:
-                world[:, :, position,:] = 1.0
-                world[:, 1:-1, :, 1:-1] = 0.0
-            else:
-                world[:, :,:,position] = 1.0
-                world[:, 1:-1, 1:-1, :] = 0.0
 
 
         #check for trigger
-        if self.channel == 4:
+        if self.trigger:
             current_volume = int(float(str(self.sound_values.buf[32:40],'utf-8')))
             if current_volume > self.lastvalue:
                 self.lastvalue = current_volume
                 self.step = 0
+                self.switch = False
+                self.stop = False
 
-            if self.step < 8:
-                self.step += 1
+            if self.type < 0.33:
+                position = 9-int(round((np.cos(0.1*self.step*self.speed)+1)*4.5))
+            elif self.type >= 0.33 and self.type < 0.66:
+                position = int(round((sawtooth(0.1*self.step*self.speed)+1)*4.5))
+            else:
+                position = int(round((sawtooth(0.1*self.step*self.speed, width=0)+1)*4.5))
 
-        if self.channel < 4:
+            if not self.stop:
+                if self.type < 0.33:
+                    nextposition = 9-int(round((np.cos(0.1*(self.step+1)*self.speed)+1)*4.5))
+                    if nextposition < position:
+                        self.switch = True
+
+                    if self.switch:
+                        if nextposition > position:
+                            self.stop = True
+
+                elif self.type >= 0.33 and self.type < 0.66:
+                    nextposition = int(round((sawtooth(0.1*(self.step+1)*self.speed)+1)*4.5))
+                    if nextposition < position:
+                        self.stop = True
+
+                else:
+                    nextposition = int(round((sawtooth(0.1*(self.step+1)*self.speed, width=0)+1)*4.5))
+                    if nextposition > position:
+                        self.stop = True
+
+                if not self.stop:
+                    self.step += 1
+
+
+        else:
+            if self.type < 0.33:
+                position = 9-int(round((np.cos(0.1*self.step*self.speed)+1)*4.5))
+            elif self.type >= 0.33 and self.type < 0.66:
+                position = int(round((sawtooth(0.1*self.step*self.speed)+1)*4.5))
+            else:
+                position = int(round((sawtooth(0.1*self.step*self.speed, width=0)+1)*4.5))
+
             self.step += 1
-            
+
+
+        if self.dir == 0:
+            world[:, position,:,:] = 1.0
+            world[:, :, 1:-1, 1:-1] = 0.0
+        elif self.dir == 1:
+            world[:, :, position,:] = 1.0
+            world[:, 1:-1, :, 1:-1] = 0.0
+        else:
+            world[:, :,:,position] = 1.0
+            world[:, 1:-1, 1:-1, :] = 0.0
+
+
         return world
