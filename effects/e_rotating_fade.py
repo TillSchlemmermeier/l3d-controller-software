1149fb711ccb7d22db8f2855e77c02c0ef47cf65
yannig@gmx.net <yannig@gmx.net>
2021-05-23 00:40:36 +0200

    added rotating_fade

---
+++ effects/e_rotating_fade.py
@@ -0,0 +1,63 @@
+# modules
+import numpy as np
+from scipy.ndimage.interpolation import rotate
+
+
+class e_rotating_black_white():
+
+    def __init__(self):
+
+        # initial rotating parameters
+        self.xspeed = 0.1
+        self.yspeed = 0.1
+        self.zspeed = 0.0
+        self.step = 0
+        self.amount = 0
+
+        self.fadeworld = np.zeros([3, 10, 10, 10])
+
+        # create gradient
+        self.colorworld = np.zeros([3, 10, 10, 10])
+
+        for i in range(10):
+            self.colorworld[:, i, :, :] = (i/9.0)**2
+
+#        self.colorworld[0, :, :, :] *= self.color['r']
+#        self.colorworld[0, :, :, :] *= self.color['g']
+#        self.colorworld[0, :, :, :] *= self.color['b']
+
+
+    #strings for GUI
+    def return_values(self):
+        return [b'rotating_black_white', b'X speed', b'Y speed', b'Z speed', b'']
+
+    def return_gui_values(self):
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(round(self.xspeed,1)), str(round(self.yspeed,1)), str(round(self.zspeed,1)), ''), 'utf-8')
+
+
+    def __call__(self, world, args):
+		# parse input
+        self.xspeed = args[0]*15+0.01
+        self.yspeed = args[1]*15
+        self.zspeed = args[2]*15
+        self.amount = args[3]
+
+        # rotate
+        newworld = rotate(self.colorworld, self.step*self.xspeed,
+                          axes = (1,2), order = 1,
+	                      mode = 'nearest', reshape = False)
+
+        newworld = rotate(newworld, self.step*self.yspeed,
+                          axes = (1,3), order = 1,
+	                      mode = 'nearest', reshape = False)
+
+        newworld = rotate(newworld, self.step*self.zspeed,
+                          axes = (2,3), order = 1,
+	                      mode = 'nearest', reshape = False)
+
+        world = self.fadeworld * newworld * amount + world
+
+        self.step += 1
+        self.fadeworld = np.clip(world, 0, 1)
+
+        return np.clip(world, 0, 1)
