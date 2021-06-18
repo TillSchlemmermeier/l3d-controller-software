0c224049f456df611ceccbac99c89f29fa6eae99
yannig@gmx.net <yannig@gmx.net>
2021-06-19 01:18:24 +0200

    added point symmetry in mirror

--- effects/e_mirror.py
+++ effects/e_mirror.py
@@ -18,9 +18,10 @@ class e_mirror():
         self.naxis = 1
         self.trigger = False
         self.steps = 0
+        self.mode = 'mirror'
 
     def return_values(self):
-        return [b'naxis', b'# of axes', b'', b'', b's2l trigger']
+        return [b'naxis', b'# of axes', b'mode', b'', b's2l trigger']
 
     def return_gui_values(self):
         if self.trigger:
@@ -28,12 +29,16 @@ class e_mirror():
         else:
             trigger = 'Off'
 
-        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(self.naxis), '', '', trigger), 'utf-8')
+        return bytearray('{0:<8s}{1:<8s}{2:<8s}{3:<8s}'.format(str(self.naxis), self.mode, '', trigger), 'utf-8')
 
     def __call__(self, world, args):
         # parsing input
         self.naxis = int(args[0]*2 +1)
         self.steps = int(args[1]*8)
+        if args[2] < 0.5:
+            self.mode = 'mirror'
+        else:
+            self.mode = 'point'
 
         if args[3] > 0.5:
             self.trigger = True
@@ -42,6 +47,8 @@ class e_mirror():
 
         tempworld = np.zeros([3, 10, 10, 10])
         tempworld[:, :, :, :] = world[:, :, :, :]
+
+
 
         if self.trigger:
             current_volume = int(float(str(self.sound_values.buf[32:40],'utf-8')))
@@ -55,16 +62,24 @@ class e_mirror():
             if self.counter > self.steps:
                 self.naxis = 0
 
-        if self.naxis == 1:
-            world[:, :, :, :] += tempworld[:, ::-1, :, :]
-        elif self.naxis == 2:
-            world[:, :, :, :] += tempworld[:, :, ::-1, :]
-            world[:, :, :, :] += tempworld[:, :, :, ::-1]
-        elif self.naxis == 3:
-            world[:, :, :, :] += tempworld[:, ::-1, :, :]
-            world[:, :, :, :] += tempworld[:, :, ::-1, :]
-            world[:, :, :, :] += tempworld[:, :, :, ::-1]
-        elif self.naxis == 0:
-            pass
+        if self.mode == 'mirror':
+
+            if self.naxis == 1:
+                world[:, :, :, :] += tempworld[:, ::-1, :, :]
+            elif self.naxis == 2:
+                world[:, :, :, :] += tempworld[:, :, ::-1, :]
+                world[:, :, :, :] += tempworld[:, :, :, ::-1]
+            elif self.naxis == 3:
+                world[:, :, :, :] += tempworld[:, ::-1, :, :]
+                world[:, :, :, :] += tempworld[:, :, ::-1, :]
+                world[:, :, :, :] += tempworld[:, :, :, ::-1]
+            elif self.naxis == 0:
+                pass
+
+        else:
+            for i in range(3):
+                tempworld[i,:,:,:] = np.rot90(tempworld[i,:,:,:], 2, (0, 1))
+
+            world[:, :, :, :] += tempworld[:, :, :, :]
 
         return np.clip(world, 0, 1)
