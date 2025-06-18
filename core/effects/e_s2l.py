fixed websocket message overflow, fixed one more AssertionError source

core/effects/e_s2l.py
@@ -22,7 +22,7 @@ class e_s2l():
         elif self.channel == 4:
             channel = "Trigger"
         elif self.channel == 5:
-            channel = 'Trig 2'
+            channel = 'Trigger/2'
         elif self.channel == 6:
             channel = 'SIN 1'
         elif self.channel == 7:
@@ -88,12 +88,12 @@ class e_s2l():
             # trigger half
             elif self.channel == 5:
 
-                # current_volume = int(float(str(self.sound_values.buf[40:48],'utf-8')))
-                #if current_volume == 1:
-                #    self.lastvalue = current_volume
-                #    self.counter = 0
+                current_volume = int(float(str(self.sound_values.buf[40:48],'utf-8')))
+                if current_volume == 1:
+                   self.lastvalue = current_volume
+                   self.counter = 0
 
-                current_volume = float(str(self.sound_values.buf[72:80],'utf-8'))
+                # current_volume = float(str(self.sound_values.buf[72:80],'utf-8'))
                 #print(current_volume)
                 world[:, :, :, :] *= (1-self.amount) + np.clip(current_volume,0,1)*self.amount
 

core/rendering_engine.py
@@ -34,14 +34,8 @@ class rendering_engine:
     def __init__(self):
         """
         Initialises the rendering engine
-
-        Keywords:
-        log : enables logging on the debug label
-              if false, the rendering engine is logging,
-              but only errors/warnings
         """
         # initialise variables
-        # self.framecounter = 1
         self.logging = False
         self.connected = False     # Arduino connection status
         self.arduino_message_shown = False
@@ -86,6 +80,9 @@ class rendering_engine:
             except IOError:
                 print('No Connection to Arduino established')
 
+        # wether to send data to arduino
+        self.should_send = False
+
         # setup empty world
         self.cubeworld = np.zeros([3, 10, 10, 10])
         self.channelworld = np.zeros([8, 3, 10, 10, 10])
@@ -103,15 +100,9 @@ class rendering_engine:
     def run(self, state):
         """generates a frame and sends the package when cube is turned on"""
         # check wether 'running' flag is set
-        # self.generate_frame(state.data.copy())
         self.generate_frame(state)
-        with state.lock:
-            state.apply_update() # Ensure we have latest state
-            should_send = state['IO']
-        if should_send:
+        if self.should_send:
             self.send_frame()
-
-        # self.framecounter += 1
 
         # Prepare combined cube data
         cube_colors = np.zeros([3, 1000])
@@ -207,6 +198,8 @@ class rendering_engine:
                     print(f"[CORE] State update failed after 3 attempts: {e}")
                     break
                 time.sleep(0.001 * retry_count)
+
+        self.should_send = snapshot['IO']
 
         # loop through channels
         for i in range(snapshot['numberOfChannels']):